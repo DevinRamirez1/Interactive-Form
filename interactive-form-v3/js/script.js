a272a34487a8a04d24e28d907b55e822e141e1ad
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-08-13 18:54:57 -0500

    Job Role
    Added variables for job role field. Added eventlistener for 'other' role.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,25 @@
-// variable to apply focus to the name input when page is opened
+//variables
 const nameFocus = document.getElementById('name');
+const jobRoleList = document.getElementById('title');
+const jobRoleOther = document.getElementById('other-job-role');
+const colorSelection = document.getElementById('color');
+
+// .focus to put focus on the name field when page is loaded
 nameFocus.focus();
+
+//hide other job textbox upon loading page
+jobRoleOther.style.display = "none";
+
+//event listener for 'other' selection
+jobRoleList.addEventListener("change", () => {
+    if (jobRoleList.value === "other"){
+        jobRoleOther.style.display = "block";
+    } else {
+        jobRoleOther.style.display = "none";
+    }
+
+});
+
+colorSelection.style.display = 'none';
+
 
