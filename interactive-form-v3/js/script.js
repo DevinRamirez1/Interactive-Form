5d2843b550364a6a02924f10fffae3d5f74f7c8f
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-08-13 22:04:47 -0500

    shirt design
    currently working on function for the shirt design options

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -3,23 +3,44 @@ const nameFocus = document.getElementById('name');
 const jobRoleList = document.getElementById('title');
 const jobRoleOther = document.getElementById('other-job-role');
 const colorSelection = document.getElementById('color');
+const shirtDesign = document.getElementById('design');
 
 // .focus to put focus on the name field when page is loaded
 nameFocus.focus();
 
 //hide other job textbox upon loading page
-jobRoleOther.style.display = "none";
+jobRoleOther.style.display = 'none';
 
 //event listener for 'other' selection
-jobRoleList.addEventListener("change", () => {
-    if (jobRoleList.value === "other"){
-        jobRoleOther.style.display = "block";
+jobRoleList.addEventListener('change', () => {
+    if (jobRoleList.value === 'other'){
+        jobRoleOther.style.display = 'block';
     } else {
-        jobRoleOther.style.display = "none";
+        jobRoleOther.style.display = 'none';
     }
 
 });
 
-colorSelection.style.display = 'none';
+colorSelection.disabled = true;
+
+shirtDesign.addEventListener('change', () => {
+    colorSelection.disabled = false;    
+    let colorOptions = colorSelection.option;
 
 
+    if (shirtDesign.value === "js puns"){
+
+        colorOptions = document.querySelectorAll('option, [data-theme*="js puns"]');
+        colorOptions[0].selected = true;
+        for (let i = 0; i <= colorOptions.length; i++){
+        colorOptions[i];
+    }
+    } else {
+
+        colorOptions = document.querySelectorAll('option, [data-theme*="heart js"]');
+        colorOptions[0].selected = true;
+        for (let i = 0; i <= colorOptions.length; i++){
+        colorOptions[i];
+        }
+    }
+})
