05182886d23f820c4f2eee6a47b687561bd78ec5
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-08-18 21:26:09 -0500

    activity and payment updates
    Added activity and payment variables. Created eventlisteners to go with them allowing changes that will also disable other choices (need to still add conflicts to activites).

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -8,8 +8,20 @@ const colorSelection = document.getElementById('color');
 const shirtDesign = document.getElementById('design');
 let designOptions = colorSelection.querySelectorAll('option')
 colorSelection.disabled = true;
-
-
+//activity selection variables
+const activityOptions = document.getElementById('activities');
+const activityCheckbox = document.querySelectorAll('.activities input');
+const totalCostParagraph = document.getElementById('activites-cost');
+//variables for payment
+const paymentOptions = document.getElementById('payment');
+const creditCard = paymentOptions.querySelector(`option[value="credit-card"]`)
+const creditCardField = document.getElementById('credit-card');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+//setting credit card as default choice and hiding other options
+creditCard.selected = 'true';
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
 // .focus to put focus on the name field when page is loaded
 nameFocus.focus();
 
@@ -51,3 +63,42 @@ shirtDesign.addEventListener('change', (e) =>{
 }
    
 });
+
+
+//event listenver for activity selection
+activityOptions.addEventListener('change', e => {
+    let clicked = e.target
+    let clickedCost = clicked.getAttribute('data-cost');
+    let clickedDateTime = clicked.getAttribute('data-day-and-time');
+    let totalCost = 0;
+
+    for (let i = 0; i < activityCheckbox.length; i ++){
+        let checkBoxes = activityCheckbox[i];
+        let checkBoxesDateTime = activityCheckbox[i].getAttribute('data-day-and-time');
+    if (checkBoxes.checked){
+        totalCost += Number(clickedCost);
+
+    }  
+}
+document.getElementById('activities-cost').textContent = `Total: $${totalCost}`;
+});
+
+paymentOptions.addEventListener('change', e =>{
+    let paymentChoice = e.target;
+
+    if (paymentChoice.value === 'bitcoin'){
+        bitcoin.style.display = 'block';
+        paypal.style.display = 'none';
+        creditCardField.style.display = 'none';
+    }
+    if (paymentChoice.value === 'paypal'){
+        bitcoin.style.display = 'none';
+        paypal.style.display = 'block';
+        creditCardField.style.display = 'none';
+    }
+    if (paymentChoice.value === 'credit-card'){
+        bitcoin.style.display = 'none';
+        paypal.style.display = 'none';
+        creditCardField.style.display = 'block';
+    }
+})
