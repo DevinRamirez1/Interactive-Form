db747262f088482748bedf8ab0c7b25187487edb
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-08-19 19:41:41 -0500

    Fixed Shirt Design; helper functions and validation
    Fixed shirt design function to select correct designs. Added helper functions for validating parts of the form. Started submit eventlistener. Need to finish.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,5 +1,8 @@
+//variable for form
+const form = document.querySelector('form');
+const submit = document.querySelector('button[type="submit"]');
 //variable for namefocus
-const nameFocus = document.getElementById('name');
+const nameInput = document.getElementById('name');
 //variables for job role
 const jobRoleList = document.getElementById('title');
 const jobRoleOther = document.getElementById('other-job-role');
@@ -18,12 +21,16 @@ const creditCard = paymentOptions.querySelector(`option[value="credit-card"]`)
 const creditCardField = document.getElementById('credit-card');
 const paypal = document.querySelector('#paypal');
 const bitcoin = document.querySelector('#bitcoin');
+//form validation variable
+const zipCode = document.getElementById('zip');
+const cvvNumber = document.getElementById('cvv');
+let activityTotal = 0
 //setting credit card as default choice and hiding other options
 creditCard.selected = 'true';
 paypal.style.display = 'none';
 bitcoin.style.display = 'none';
 // .focus to put focus on the name field when page is loaded
-nameFocus.focus();
+nameInput.focus();
 
 //hide other job textbox upon loading page
 jobRoleOther.style.display = 'none';
@@ -41,14 +48,14 @@ jobRoleList.addEventListener('change', () => {
 
 // event listener for selecting shirt design
 shirtDesign.addEventListener('change', (e) =>{
-    let design = e.target.value;
+
     colorSelection.disabled = false;
     for (let i = 0; i < designOptions.length; i++){
         designOptions[i].hidden = true;
+    
     }
 
-
-    if (design.value === "js puns") {
+    if (shirtDesign.value === "js puns") {
         designOptions = colorSelection.querySelectorAll('[data-theme="js puns"]');
         designOptions[0].selected = true
         for (let i = 0; i < designOptions.length; i++){
@@ -61,7 +68,7 @@ shirtDesign.addEventListener('change', (e) =>{
             designOptions[i].hidden = false;
     }
 }
-   
+
 });
 
 
@@ -77,11 +84,17 @@ activityOptions.addEventListener('change', e => {
         let checkBoxesDateTime = activityCheckbox[i].getAttribute('data-day-and-time');
     if (checkBoxes.checked){
         totalCost += Number(clickedCost);
-
+        
     }  
 }
+(e.target.checked) ? activityTotal++ : activityTotal--;
 document.getElementById('activities-cost').textContent = `Total: $${totalCost}`;
+console.log(activityTotal);
+console.log(totalCost);
+console.log(clickedCost);
 });
+
+
 
 paymentOptions.addEventListener('change', e =>{
     let paymentChoice = e.target;
@@ -101,4 +114,52 @@ paymentOptions.addEventListener('change', e =>{
         paypal.style.display = 'none';
         creditCardField.style.display = 'block';
     }
-})
+});
+
+//helper functions for form validation
+const nameValidator = () => {
+    const nameValue = nameInput.value;
+    const nameValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
+    return nameValid;
+};
+
+const emailValidator = () => {
+    const emailValue = nameInput.value;
+    const emailValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+    return emailValid;
+};
+
+const activityValidator = () => {
+    const activityValid = activityTotal > 0
+    return activityValid;
+};
+
+const creditCardValidator = () => {
+    const creditCardValue = creditCard.value;
+    const creditCardValid = /^[\d]{13,16}$/.test(creditCardValue);
+    const zipCodeValue = zipCode.value;
+    const zipCodeValid = /^[\d]{5}$/.test(zipCodeValue);
+    const cvvValue = cvvNumber.value;
+    const cvvValid = /^[\d]{3}$/.test(cvvValue);
+};
+
+//submit eventlistener
+submit.addEventListener('click', () => {
+    nameValidator();
+    emailValidator();
+    activityValidator();
+    creditCardValidator();
+
+    if (!nameValidator()) {
+        e.preventDefault();
+      }
+      if (!emailValidator()) {
+        e.preventDefault();
+      }
+      if (!activityValidator()) {
+        e.preventDefault();
+      }
+      //if (!creditCardValidator()) {
+       // e.preventDefault();
+     // }
+});
